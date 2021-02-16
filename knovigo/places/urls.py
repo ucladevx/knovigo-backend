648b798bc1923059e989aacb37231b611f665cad
Athya <athyauk@gmail.com>
2021-02-16 12:11:04 -0800

    Added float parameters to location endpoint

--- knovigo/places/urls.py
+++ knovigo/places/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, register_converter
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from . import views, scraper
+from . import converters, views, scraper
 
 router = routers.DefaultRouter()
 router.register(r'places', views.PopularTimesViewSet)
-
+register_converter(converters.FloatUrlParameterConverter, 'float')
 # # Wire up our API using automatic URL routing.
 # # Additionally, we include login URLs for the browsable API.
 # urlpatterns = [
@@ -17,7 +17,7 @@ router.register(r'places', views.PopularTimesViewSet)
 urlpatterns = [
 	path('all', views.place_list),
     path('filtered', views.place_filter_list),
-    path('location', views.place_location_list),
+    path('location/<float:latitude>/<float:longitude>', views.place_location_list),
     path('place/<id>', views.place_detail),
     path('get_user_report', views.get_user_report_data),
     path('get_place_data', views.get_place_data_updates),
--- knovigo/places/views.py
+++ knovigo/places/views.py
@@ -16,43 +16,34 @@ from .models import PopularTimes
 from geopy.distance import geodesic
 from django.db.models import F
 
-# class PlaceViewSet(viewsets.ModelViewSet):
-#     queryset = Place.objects.all().order_by('name')
-#     serializer_class = PlaceSerializer
-
-# class BusinessHoursViewSet(viewsets.ModelViewSet):
-#     queryset = BusinessHours.objects.all()
-#     serializer_class = BusinessHoursSerializer
-
 class PopularTimesViewSet(viewsets.ModelViewSet):
-    queryset = PopularTimes.objects.all()
-    serializer_class = PopularTimesSerializer
+	queryset = PopularTimes.objects.all()
+	serializer_class = PopularTimesSerializer
 
 def place_detail(request, id):
-    """
-    Get details for a place
-    """
-    try:
-        place = Place.objects.get(place_id=id)
-    except Place.DoesNotExist:
-        return HttpResponse(status=404)
+	"""
+	Get details for a place
+	"""
+	try:
+		place = Place.objects.get(place_id=id)
+	except Place.DoesNotExist:
+		return HttpResponse(status=404)
 
-    if request.method == 'GET':
-        serializer = PlaceSerializer(place)
-        return JsonResponse(serializer.data)
+	if request.method == 'GET':
+		serializer = PlaceSerializer(place)
+		return JsonResponse(serializer.data)
 
-def place_location_list(request):
+
+# Added latitude and longitude parameters. TODO: add distance calculation
+def place_location_list(request, latitude, longitude):
 	"""
 	List all places - ratings, name, distance, expensiveness, types, safety data
 	"""
-	location = {
-		'lat': 34.0611873,
-		'long': -118.4469309
-	}
 	if request.method == 'GET':
 		fields = ("place_id", "address", "name", "businessHours", "rating", "rating_n", "price_level", "types", "latitude", "longitude", "agg_density", "agg_social", "agg_mask")#, "distance")
-		#places = Place.objects.all().only(*fields).order_by("distance") # refine order by filter with more data
-		places = Place.objects.all().annotate(distance=geodesic((F('latitude'), F('longitude')), (34.0611873, -118.4469309)).miles).order_by("distance")
+		# places = Place.objects.all().only(*fields).order_by("distance") # refine order by filter with more data
+		# places = Place.objects.all().annotate(distance=geodesic((F('latitude'), F('longitude')), (34.0611873, -118.4469309)).miles).order_by("distance")
+		places = Place.objects.all()
 		serializer = PlaceSerializer(places, many=True, context={'request': request}, fields = fields)
 		return JsonResponse(serializer.data, safe=False)
 
@@ -67,18 +58,18 @@ def place_filter_list(request):
 		return JsonResponse(serializer.data, safe=False)
 
 def place_list(request):
-    """
-    List all places with all information
-    """
-    if request.method == 'GET':
-        places = Place.objects.all()
-        serializer = PlaceSerializer(places, many=True, context={'request': request})
-        return JsonResponse(serializer.data, safe=False)
-      
+	"""
+	List all places with all information
+	"""
+	if request.method == 'GET':
+		places = Place.objects.all()
+		serializer = PlaceSerializer(places, many=True, context={'request': request})
+		return JsonResponse(serializer.data, safe=False)
+	  
 def get_place_data_updates(request):
-    update_place_data()
-    return JsonResponse({'success': True})
+	update_place_data()
+	return JsonResponse({'success': True})
 
 def get_user_report_data(request):
-    count = scrape_user_report_data()
-    return JsonResponse({'count': count})
+	count = scrape_user_report_data()
+	return JsonResponse({'count': count})
